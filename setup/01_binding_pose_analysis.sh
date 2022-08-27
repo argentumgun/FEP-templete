a5d2bbfcd6f543c013a2524829c4d046d539bf58
argentumgun <q1154678587@gmail.com>
2022-08-27 23:25:10 +0800

    gauss+setup

--- setup/01_binding_pose_analysis.sh
+++ setup/01_binding_pose_analysis.sh
@@ -27,13 +27,14 @@ for pose in 1; do
     echo "current working folder:"
     pwd
     
-    ligand="ligand_pose_$pose.pdb"
+    ligand="ligand_pose_$pose"
     echo "binding ligand : $ligand"
 
+    echo "$(date "+%Y-%m-%d %H:%M:%S") GAUSS start"
+    g16 $top/binding_pose_dir/$ligand.gjf
+    #antechamber -i $top/binding_pose_dir/$ligand -fi pdb -o lig.com -fo gcrt
     echo "$(date "+%Y-%m-%d %H:%M:%S") antechamber start"
-
-    antechamber -i $top/binding_pose_dir/$ligand -fi pdb -o lig.com -fo gcrt
-    antechamber -i lig.com -fi gcrt -o lig.mol2 -fo mol2 -c bcc -at gaff2
+    antechamber -i $top/binding_pose_dir/$ligand.log -fi gout -o lig.mol2 -fo mol2 -c bcc -at gaff2
     parmchk2 -i lig.mol2 -f mol2 -o lig.frcmod -s gaff2
 
     echo "running tleap"
