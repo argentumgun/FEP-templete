75e1685a32944b18f31b23c05731956c035f82d0
argentumgun <q1154678587@gmail.com>
2023-04-19 19:39:59 +0800

    master

--- free_energy/run.sh
+++ free_energy/run.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 # Run script1.sh
+echo "SET UP"
 nohup sh ./script/01_set_up.sh > logs/01_set_up.log 2>&1 &
 
+echo "sleep 20 S" 
 sleep 20
 
+echo "run minimization"
 nohup sh ./script/02_restraint_minization.sh > logs/02_restraint_minization.log 2>&1 &
 nohup sh ./script/03_complex_0.0_minimization.sh > logs/03_complex_0.0_minimization.log 2>&1 &
 nohup sh ./script/04_complex_0.5_minimization.sh > logs/04_complex_0.5_minimization.log 2>&1 &
 nohup sh ./script/05_ligand_minimization.sh > logs/05_ligand_minimization.log 2>&1 &
 
+echo "sleep 1000 S"
 sleep 1000
 
+echo "run pmemd.cuda"
 nohup sh ./script/06_restraint_pmemd.sh > logs/06_restraint_pmemd.log 2>&1 &
 nohup sh ./script/07_complex_0.0_pmemd.sh > logs/07_complex_0.0_pmemd.log 2>&1 &
 nohup sh ./script/08_complex_0.5_pmemd.sh > logs/08_complex_0.5_pmemd.log 2>&1 &
