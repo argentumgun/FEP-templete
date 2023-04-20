1077e864a3c3b134e5158a4de7b538cf95a93803
argentumgun <q1154678587@gmail.com>
2023-04-20 13:09:03 +0800

    MASTER

--- setup/03_select_restraint_set.sh
+++ setup/03_select_restraint_set.sh
@@ -24,4 +24,9 @@ cd ${top}/binding_pose_MD/${pose}/analysis/$system_restraint
 
 sed  -e "s/%A%/$ATOM_A/g" -e "s/%B%/$ATOM_B/g" -e "s/%C%/$ATOM_C/g" -e "s/%a%/$ATOM_a/g" -e "s/%b%/$ATOM_b/g" -e "s/%c%/$ATOM_c/g" $top/run_protocol/restraint_analysis.tmpl > $system_restraint.in
 
+sed -e "s/%PA%/$ATOM_A/g" -e "s/%PB%/$ATOM_B/g" -e "s/%PC%/$ATOM_C/g" -e "s/%La%/$ATOM_a/g" -e "s/%Lb%/$ATOM_b/g" -e "s/%Lc%/$ATOM_c/g" $top/run_protocol/05_set_boresch_restraint.sh.tmpl > 05_set_boresch_restraint.sh
+
+sed -e "s/%PA%/$ATOM_A/g" -e "s/%PB%/$ATOM_B/g" -e "s/%PC%/$ATOM_C/g" -e "s/%La%/$ATOM_a/g" -e "s/%Lb%/$ATOM_b/g" -e "s/%Lc%/$ATOM_c/g" ../free_energy/script/06_restraint_pmemd.sh.tmpl > ../free_energy/script/06_restraint_pmemd.sh
+
+
 cpptraj -i $system_restraint.in > $system_restraint.log
